ca2bed0e78cdbddb22c27caabe479da81905dbe6
nimiabaga <nimiabaga@yahoo.com>
2025-07-29 09:38:20 +0100

    project done

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        gold: '#AB8000',
+        deepblue: '#0C1220',
+        slateblue: '#192535'
+      },
+      fontFamily: {
+        opensans: ['Open Sans', 'sans-serif'],
+        merriweather: ['Merriweather', 'serif'],
+      }
+    },
+  },
+  plugins: [],
+}
